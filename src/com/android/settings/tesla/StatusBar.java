b7b82981184cf8f609b6d3c6be9aae98c650c56f
theringmaster <gzroms@hotmail.com>
2016-02-20 10:30:47 -0500

    Fixes

--- src/com/android/settings/tesla/StatusBar.java
+++ src/com/android/settings/tesla/StatusBar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 SlimRoms Project
+ * Copyright (C) 2016 Tesla Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,45 +16,54 @@
 
 package com.android.settings.tesla;
 
+import android.content.Context;
 import android.content.ContentResolver;
-import android.database.ContentObserver;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.Handler;
-import android.preference.ListPreference;
+import android.os.UserHandle;
+import android.preference.SwitchPreference;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
-import android.provider.Settings.SettingNotFoundException;
-import android.util.Log;
 
-import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsLogger;
 import com.android.settings.Utils;
 
 public class StatusBar extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
-    private static final String TAG = "StatusBarSettings";
+    private static final String PREF_CUSTOM_HEADER = "status_bar_custom_header";
+    private static final String PREF_CUSTOM_HEADER_DEFAULT = "status_bar_custom_header_default";
 
-    @Override
-    protected int getMetricsCategory() {
-        // todo add a constant in MetricsLogger.java
-        return MetricsLogger.MAIN_SETTINGS;
-    }
+    private SwitchPreference mCustomHeader;
+    private SwitchPreference mCustomHeaderDefault;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.status_bar_settings);
+        PreferenceScreen prefSet = getPreferenceScreen();
 
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mCustomHeader = (SwitchPreference) prefSet.findPreference(PREF_CUSTOM_HEADER);
+        mCustomHeader.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1));
+        mCustomHeader.setOnPreferenceChangeListener(this);
+
+        mCustomHeaderDefault = (SwitchPreference) prefSet.findPreference(PREF_CUSTOM_HEADER_DEFAULT);
+        mCustomHeaderDefault.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT, 0) == 1));
+        mCustomHeaderDefault.setOnPreferenceChangeListener(this);
     }
 
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.APPLICATION;
     }
 
     @Override
@@ -62,4 +71,25 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
         super.onResume();
     }
 
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    ContentResolver resolver = getActivity().getContentResolver();
+      if (preference == mCustomHeader) {
+         Settings.System.putInt(getContentResolver(),
+                 Settings.System.STATUS_BAR_CUSTOM_HEADER,
+                 (Boolean) newValue ? 1 : 0);
+         return true;
+      } else if (preference == mCustomHeaderDefault) {
+         Settings.System.putInt(getContentResolver(),
+                 Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT,
+                 (Boolean) newValue ? 1 : 0);
+         return true;
+       }
+       return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
 }
